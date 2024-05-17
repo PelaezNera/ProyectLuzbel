0822a10f1faf624342a63a4878436574fd9f0999
JuanManuelG11 <juanmgv11@gmail.com>
2024-05-17 16:56:26 -0500

    validar sesiones

--- main-pages/logear.php
+++ main-pages/logear.php
@@ -24,7 +24,7 @@ if (mysqli_num_rows($verificar) > 0) {
 
 $resultado = mysqli_query($conexion,$insertar);
 if ($resultado) {
-    echo "<script>alert('El usuario $user se a registrado con exito'); window.location.href = '../pagina.html'</script>";
+    echo "<script>alert('El usuario $user se a registrado con exito'); window.location.href = '../pagina.php'</script>";
 } else {
     echo "<script>alert('Problema al insertar el usuario por favor intente otra vez'); window.location.href = 'login.html'</script>";
 }
--- panel_admin.php
+++ panel_admin.php
@@ -1,3 +1,14 @@
+<?php
+include 'main-pages/conexion.php';
+
+session_start();
+$var_session = $_SESSION['usuario'];
+if ($var_session == null || $var_session == " ") {
+    echo "<script>window.alert('No tienes autorizacion para ingresar'); window.location.href = './main-pages/login.html'</script>";
+    die();
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
