b4e922caa790fc3b65e3242c871f510ed36e3ea5
JoseLasCalvea <114762963+JoseLasCalvea@users.noreply.github.com>
2024-05-23 17:04:08 -0500

    pagina admin

--- panel_admin.php
+++ panel_admin.php
@@ -11,12 +11,109 @@ if ($var_session == null || $var_session == " ") {
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Admin</title>
+  <link rel="stylesheet" href="normalize.css">
+  <link rel="stylesheet" href="frontend/style2.css">
 </head>
+
 <body>
-    <h1>Admin</h1>
+
+  <header>
+    <nav class="nav-header">
+      <img src="img/logotest.png" alt="">
+      <h1>
+        Hallucination Studios
+      </h1>
+      <a href="#Inicio">Inicio</a>
+      <a href="#Info">Information</a>
+      <a href="#Noticias">Noticias</a>
+      <a href="#User">Usuario</a>
+      <a href="backend/cerrar_sesion.php">Cerrar sesion</a>
+    </nav>
+  </header>
+
+    <hr class="divisor">
+    <section class="info" id="Info">
+    <div class="articles">
+      <article>
+          <figure>
+              <img src="img/6.jpg" alt="Lavender Fields">
+          </figure>
+          <div class="article-preview">
+              <h2>La Nada</h2>
+              <p>
+                  Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+                  Praesent in mauris eu tortor porttitor accumsan.
+              </p>
+          </div>
+      </article>
+      <article>
+          <figure>
+              <img src="img/5.jpg" alt="Snowy Mountains">
+          </figure>
+          <div class="article-preview">
+              <h2>Arcangel San Miguel</h2>
+              <p>
+                  Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+                  Praesent in mauris eu tortor porttitor accumsan.
+              </p>
+          </div>
+      </article>
+      <article>
+          <figure>
+              <img src="img/2.jpg" alt="Wooden Bridge">
+          </figure>
+          <div class="article-preview">
+              <h2>El Suplente</h2>
+              <p>
+                  Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+                  Praesent in mauris eu tortor porttitor accumsan.
+              </p>
+          </div>
+      </article>
+      <article>
+          <figure>
+              <img src="img/3.jpg" alt="Autumn Forest">
+          </figure>
+          <div class="article-preview">
+              <h2>Common Protector</h2>
+              <p>
+                  Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Praesent in mauris eu tortor porttitor
+                  accumsan.
+              </p>
+          </div>
+      </article>
+      <article>
+          <figure>
+              <img src="img/4.jpg" alt="Freezing Forest">
+          </figure>
+          <div class="article-preview">
+              <h2>Gate Protector</h2>
+              <p>
+                  Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Praesent in mauris eu tortor porttitor
+                  accumsan.
+              </p>
+          </div>
+      </article>
+
+      <article>
+          <figure>
+              <img src="img/4.jpg" alt="Freezing Forest">
+          </figure>
+          <div class="article-preview">
+              <h2>Gate Protector</h2>
+              <p>
+                  Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Praesent in mauris eu tortor porttitor
+                  accumsan.
+              </p>
+          </div>
+      </article>
+    </div>
+    </section>
 </body>
+
 </html>
