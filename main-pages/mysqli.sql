8fdacd8af6d9bf00828f40e6f4dc9268aea91b02
josuel-munendez <jose.munozf@iejosemariabravomarquez.edu.co>
2024-05-31 14:43:00 -0500

    mejora al sql y css

--- main-pages/mysqli.sql
+++ main-pages/mysqli.sql
@@ -3,13 +3,14 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 19-05-2024 a las 22:11:40
+-- Tiempo de generación: 31-05-2024 a las 01:09:03
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.2.4
 
 --
 -- COMMENT 'Base de datos que usaremos para nuestro proyecto de juego y pagina'
 --
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -17,50 +18,92 @@ SET time_zone = "+00:00";
 -- Base de datos: `juego`
 --
 CREATE DATABASE IF NOT EXISTS `juego` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-
--- SELECT DATABASE juego; -- Esta  es otra forma de seleccionar
-USE juego;
+USE `juego`;
 
 -- --------------------------------------------------------
 
 --
 -- Estructura de tabla para la tabla `usuarios`
 --
--- Creación: 19-05-2024 a las 19:56:39
--- Última actualización: 19-05-2024 a las 20:00:39
+-- Creación: 19-05-2024 a las 20:33:56
+-- Última actualización: 30-05-2024 a las 22:15:38
 --
 
 DROP TABLE IF EXISTS `usuarios`;
-CREATE TABLE
-    IF NOT EXISTS `usuarios` (
-        `id_usuario` INT(5) NOT NULL AUTO_INCREMENT COMMENT 'El distintor e identificador irrepetible de cada usuario', -- PRIMARY KEY
-        -- `nombre` VARCHAR(100) NOT NULL COMMENT 'El nombre real personal del usuario',
-        `email` VARCHAR(100) NOT NULL COMMENT 'El correo electrónico de contacto del usuario',
-        `usuario` VARCHAR(50) NOT NULL COMMENT 'El apodo y nombre cuenta del usuario',
-        `pass` VARCHAR(20) NOT NULL COMMENT 'El verificador o código de ingreso de la cuenta del usuario',
-        `tipo` VARCHAR(10) NOT NULL COMMENT 'El tipo de cuenta y Rol o Cargo del usuario dentro de la pagina, lo que definira su nivel de acceso',
-        PRIMARY KEY (`id_usuario`),
-        UNIQUE (`usuario`, `email`)
-    ) ENGINE = InnoDB CHARSET = utf32 COLLATE utf32_spanish_ci COMMENT = 'Tabla para guardar los datos de los usuarios';
+CREATE TABLE IF NOT EXISTS `usuarios` (
+  `id_usuario` int(5) NOT NULL AUTO_INCREMENT COMMENT 'El distintor e identificador irrepetible de cada usuario',
+  `email` varchar(100) NOT NULL COMMENT 'El correo electrónico de contacto del usuario',
+  `usuario` varchar(50) NOT NULL COMMENT 'El apodo y nombre cuenta del usuario',
+  `pass` varchar(20) NOT NULL COMMENT 'El verificador o código de ingreso de la cuenta del usuario',
+  `tipo` varchar(10) NOT NULL COMMENT 'El tipo de cuenta y Rol o Cargo del usuario dentro de la pagina, lo que definira su nivel de acceso',
+  PRIMARY KEY (`id_usuario`),
+  UNIQUE KEY `usuario` (`usuario`,`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf32 COLLATE=utf32_spanish_ci COMMENT='Tabla para guardar los datos de los usuarios';
 
 --
--- Importación de datos para la tabla `usuarios`
+-- RELACIONES PARA LA TABLA `usuarios`:
 --
 
-INSERT INTO `usuarios` (`id_usuario`, `email`, `usuario`, `pass`, `tipo`) VALUES
-(1, 'fernandezjosemanuel524@gmail.com', 'Jose Manuel', '1111', 'user');
+--
+-- Truncar tablas antes de insertar `usuarios`
+--
+
+TRUNCATE TABLE `usuarios`;
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT DELAYED IGNORE INTO `usuarios` (`id_usuario`, `email`, `usuario`, `pass`, `tipo`) VALUES
+(1, 'fernandezjosemanuel524@gmail.com', 'Jose Manuel', '11111', 'user'),
+(2, 'marina@Basquetball.com', 'marina Grande ', '12345', 'user');
 
 
 --
 -- Metadatos
 --
-USE phpmyadmin;
+USE `phpmyadmin`;
 
 --
 -- Metadatos para la tabla usuarios
 --
 
 --
+-- Truncar tablas antes de insertar `pma__column_info`
+--
+
+TRUNCATE TABLE `pma__column_info`;
+--
+-- Truncar tablas antes de insertar `pma__table_uiprefs`
+--
+
+TRUNCATE TABLE `pma__table_uiprefs`;
+--
+-- Truncar tablas antes de insertar `pma__tracking`
+--
+
+TRUNCATE TABLE `pma__tracking`;
+--
 -- Metadatos para la base de datos juego
 --
+
+--
+-- Truncar tablas antes de insertar `pma__bookmark`
+--
+
+TRUNCATE TABLE `pma__bookmark`;
+--
+-- Truncar tablas antes de insertar `pma__relation`
+--
+
+TRUNCATE TABLE `pma__relation`;
+--
+-- Truncar tablas antes de insertar `pma__savedsearches`
+--
+
+TRUNCATE TABLE `pma__savedsearches`;
+--
+-- Truncar tablas antes de insertar `pma__central_columns`
+--
+
+TRUNCATE TABLE `pma__central_columns`;
 COMMIT;
