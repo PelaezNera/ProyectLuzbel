eb5fe1e7fd95abf03e7498dcb9d28ebe037da7ad
PelaezNera <losgrizz23@gmail.com>
2024-10-24 15:20:10 -0500

    sdfghj

--- panel_admin/actualizar_tarjetas.php
+++ panel_admin/actualizar_tarjetas.php
@@ -1,3 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Actualizar</title>
+    <script src="../sweetalert2@11.js"></script>
+</head>
+<body>
+
 <?php
 include '../main-pages/conexion.php';
 
@@ -12,14 +22,41 @@ if ($imagen == "") {
     $resultado = mysqli_query($conexion,$consulta);
     
     if ($resultado) {
-        echo "<script>alert('Se actualizaron correctamente los datos'); window.location='./gestion_foro.php'</script>";
+        echo '<script>
+        Swal.fire({
+        position: "center",
+        icon: "success",
+        title: "Actualizado",
+        showConfirmButton: false,
+        timer: 1500
+        }).then(function() {
+        window.location = "../panel_admin/gestion_foro.php";});
+        </script>';
     } else {
-        echo "<script>alert('Problemas para actualizar los datos'); window.location='./gestion_foro.php'</script>";
+        echo '<script>
+        Swal.fire({
+        position: "center",
+        icon: "error",
+        title: "Eliminar",
+        showConfirmButton: false,
+        timer: 1500
+        }).then(function() {
+        window.location = "../panel_admin/gestion_foro.php";});
+        </script>';
     }
 }
 
 if ($imagen == "" || $titulo == "" || $informacion == "") {
-    echo "<script>alert('Campos sin completar'); window.location='./gestion_foro.php'</script>";
+    echo '<script>
+    Swal.fire({
+    position: "center",
+    icon: "error",
+    title: "Your work has been saved",
+    showConfirmButton: false,
+    timer: 1500
+    }).then(function() {
+    window.location = "../panel_admin/gestion_foro.php";});
+    </script>';
 } else{
 
     $consulta = ("UPDATE informacion SET titulo = '$_POST[titulo]', informacion = '$_POST[informacion]', imagen = '$_POST[imagen]' WHERE id_noticia = '$codigo'");
@@ -27,8 +64,30 @@ if ($imagen == "" || $titulo == "" || $informacion == "") {
     $resultado = mysqli_query($conexion,$consulta);
     
     if ($resultado) {
-        echo "<script>alert('Se actualizaron correctamente los datos'); window.location='./gestion_foro.php'</script>";
+        echo '<script>
+        Swal.fire({
+        position: "center",
+        icon: "success",
+        title: "Actualizado",
+        showConfirmButton: false,
+        timer: 1500
+        }).then(function() {
+        window.location = "../panel_admin/gestion_foro.php";});
+        </script>';
     } else {
-        echo "<script>alert('Problemas para actualizar los datos'); window.location='./gestion_foro.php'</script>";
+        echo '<script>
+        Swal.fire({
+            position: "center",
+        icon: "error",
+            title: "Mal",
+        showConfirmButton: false,
+        timer: 1500
+        }).then(function() {
+        window.location = "../panel_admin/gestion_foro.php";});
+        </script>';
     }
 } ?>
+    
+</body>
+</html>
+
